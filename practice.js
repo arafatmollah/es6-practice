30607243b7a648958e3ed8a1039381ca3c575606
Arafat MShuvo <arafatshuvo18@gmail.com>
2022-04-14 12:00:58 +0600

    two practice done

--- practice.js
+++ practice.js
@@ -5,6 +5,15 @@ console.log('let can re-assign', arafat);
 const nature = 'green is the main value of the green';
 console.log('const can not be changed', nature);
 
+const address = {
+    name: 'Arafat',
+    hobby: 'web development',
+    expected: 40000,
+    goal: 6,
+    role: 'software company'
+}
+//console.log(address)
+
 const fulltemplate = `Hello from arafat. It is the main thing's that arafat can do
-${nature}. Agin said that ${arafat}. Okay I Think I finished no, 2`;
-console.log(fulltemplate)
+${nature}. Agin said that ${arafat}. Okay I Think I finished no :${address.name}, ${address.expected}`;
+console.log(fulltemplate);
