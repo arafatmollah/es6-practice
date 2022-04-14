c0a8c571a1cd98cc78e12dc813103b9dcd0b1772
Arafat MShuvo <arafatshuvo18@gmail.com>
2022-04-14 11:51:42 +0600

    practice

---
+++ practice.js
@@ -0,0 +1,10 @@
+let arafat = 'this is my full name. are you sure';
+arafat = 'in the previous i say that, this is my full name. are you sure';
+console.log('let can re-assign', arafat);
+
+const nature = 'green is the main value of the green';
+console.log('const can not be changed', nature);
+
+const fulltemplate = `Hello from arafat. It is the main thing's that arafat can do
+${nature}. Agin said that ${arafat}. Okay I Think I finished no, 2`;
+console.log(fulltemplate)
